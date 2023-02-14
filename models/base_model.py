f1858d412850ac8ffba7939752702f4091f32f9c
erickson mbuvi <mutisyaerickson@gmail.com>
2023-02-14 15:37:55 +0300

    started the base model

---
+++ models/base_model.py
@@ -0,0 +1,41 @@
+"""this module contains the base module class
+"""
+
+
+import uuid
+import datetime
+
+class BaseModel():
+    """this is the base model class 
+
+    Attributes:
+        id(string): an uuid assigned when instance is created
+        created_at(datetime): current datetime when an instance is created
+        updated_at(datetime): current datetime when an instance is created
+                                and it will be updated every time you 
+                                change your object
+
+    __init__: initializes the class attributes
+
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at  = datetime.datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """updates the public instance attribute updated_at with
+        the current datetime
+
+        """
+        updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        the_dict = self.__dict__
+        the_dict['__class__'] = self.__class__.__name__
+        the_dict['created_at'] = self.created_at.isoformat()
+        the_dict['updated_at'] = self.updated_at.isoformat()
+        return (the_dict)
