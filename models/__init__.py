f0239f3d874667cf1935829fc89a3ee8944f37c8
erickson mbuvi <mutisyaerickson@gmail.com>
2023-02-25 01:23:30 +0300

    fixing the base model

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from models import file_storage as fs
+
+storage = fs.FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 import uuid
 import datetime
 from dateutil import parser
+from models import storage
 
 class BaseModel():
     """this is the base model class 
@@ -32,6 +33,9 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at  = datetime.datetime.now()
             self.updated_at = self.created_at
+        if args:
+            for obj in args:
+                storage.new(obj)
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
@@ -42,6 +46,7 @@ class BaseModel():
 
         """
         updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         the_dict = self.__dict__
